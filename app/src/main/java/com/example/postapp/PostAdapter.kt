67ea67862b0b9f22869c60100c16b05b2c97fb3c
epiloksaachdan <epiloksaachdan@gmail.com>
2021-07-15 17:47:51 +0700

    Creating UI

---
+++ app/src/main/java/com/example/postapp/PostAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.postapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+    inner class PostViewHolder(v: View) : RecyclerView.ViewHolder(v){
+        fun bind(){
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
---
+++ app/src/main/java/com/example/postapp/database/Post.kt
@@ -0,0 +1,3 @@
+package com.example.postapp.database
+
+data class Post{}
